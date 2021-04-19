cc1cf129e39ad315b3fa4b6901c57a7916d12144
Gustavo Azevedo <gustavosazevedo@gmail.com>
2021-04-19 18:23:41 -0300

    add photos upload

--- public/scripts.js
+++ public/scripts.js
@@ -13,3 +13,104 @@ const Mask = {
     }).format(value/100)
   }
 }
+
+const PhotosUpload = {
+  input: "",
+  uploadLimit: 6,
+  preview: document.querySelector('#photos-preview'),
+  files: [],
+  handleFileInput(event) {
+    const { files: fileList } = event.target
+    PhotosUpload.input = event.target
+    
+    if (PhotosUpload.hasLimit(event)) return
+     
+    Array.from(fileList).forEach(file => { 
+
+      PhotosUpload.files.push(file)
+
+      const reader = new FileReader() // Formato blob
+
+      reader.onload = () => {
+        const image = new Image()
+        image.src = String(reader.result)
+        
+        const div = PhotosUpload.getContainer(image)
+
+        PhotosUpload.preview.appendChild(div)
+      }
+
+      reader.readAsDataURL(file)
+    })
+
+    PhotosUpload.input.files = PhotosUpload.getAllFiles()
+  },
+  hasLimit(event) {
+    const { uploadLimit, input, preview } = PhotosUpload
+    const { files: fileList } = input
+
+    if (fileList.length > uploadLimit) { 
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefaulft()
+      return true
+    }
+
+    const photosDiv = []
+    console.log(preview.childNodes)
+    preview.childNodes.forEach(item => {
+      if (item.classList && item.classList.value == "photo") {
+        photosDiv.push(item)
+      }
+    })
+
+    const totalPhotos = fileList.length + photosDiv.length 
+    if (totalPhotos > uploadLimit) {
+      alert("Você atingiu o limite máximo de fotos")
+      event.preventDefaulft()
+      return true
+    }
+    
+    return false
+  },
+  getAllFiles() {
+    const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+    PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+    
+    console.log(dataTransfer) // Retorna um objeto DataTransfer que uma das coisas dentro dele, é um files. E dentro desse files, ta uma fileList com os arquivos selecionados
+
+    return dataTransfer.files
+  },
+  getContainer(image) {
+    const div = document.createElement('div')
+      div.classList.add('photo')
+
+      div.onclick = PhotosUpload.removePhoto
+      // div.onclick = (event) => { 
+      //   PhotosUpload.removePhoto(event)
+      // }
+
+      div.appendChild(image)
+
+      div.appendChild(PhotosUpload.getRemoveButton())
+
+      return div
+  },
+  getRemoveButton() {
+    const button = document.createElement('i')
+    button.classList.add('material-icons')
+    button.innerHTML = "close"
+
+    return button
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode
+    const photosArray = Array.from(PhotosUpload.preview.children)
+    const index = photosArray.indexOf(photoDiv)
+
+    PhotosUpload.files.splice(index, 1)
+    PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+    photoDiv.remove()
+  }
+}
